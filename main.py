1e1bce62bf2a00f13cc443603e3ee1680cf8dd79
kaganndemirr <kaganndemirr@gmail.com>
2023-05-14 17:33:45 +0300

    final version

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
 import os
 import sys
 import numpy as np
-from multiprocessing import Value, Array
+from multiprocessing import Array
 
-def create_random_matrix(size):
-    return np.random.randint(1, 11, (size, size, size))
+def matrix_sum_worker(start, size, matrix1, matrix2, result):
+    for z in range(start, size, process_count):
+        for y in range(size):
+            for x in range(size):
+                result[z*size*size + y*size + x] = matrix1[z][y][x] + matrix2[z][y][x]
 
-def matrix_sum_worker(start, end, matrix1, matrix2, result, size):
-    for idx in range(start, end):
-        i = idx // (size * size)
-        j = (idx % (size * size)) // size
-        k = idx % size
-        result[idx] = matrix1[i][j][k] + matrix2[i][j][k]
+if __name__ == '__main__':
+    size = 2
+    process_count = 1
 
-def main():
-    size = 500
+    matrix1 = np.random.randint(1, 11, size=(size, size, size))
+    matrix2 = np.random.randint(1, 11, size=(size, size, size))
 
-    matrix1 = create_random_matrix(size)
-    matrix2 = create_random_matrix(size)
-
-    process_count = 8
-    total_elements = size * size * size
-    elements_per_process = (total_elements + process_count - 1) // process_count
-
-    result = Array('i', total_elements)
+    result = Array('i', size*size*size, lock=False)
 
     processes = []
-    for p in range(process_count):
-        start = p * elements_per_process
-        end = min((p + 1) * elements_per_process, total_elements)
-
+    for process_num in range(process_count):
         pid = os.fork()
-        if pid == 0:
-            matrix_sum_worker(start, end, matrix1, matrix2, result, size)
-            sys.exit(0)
-        elif pid > 0:
+        if pid > 0:
             processes.append(pid)
+        elif pid == 0:
+            matrix_sum_worker(process_num, size, matrix1, matrix2, result)
+            sys.exit(0)
         else:
-            print("Fork failed")
+            print(f"Fork failed with error code: {pid}")
             sys.exit(1)
 
-    for pid in processes:
-        os.waitpid(pid, 0)
+    for p in processes:
+        os.waitpid(p, 0)
 
-    result_matrix = np.array(result[:]).reshape(size, size, size)
-    matrix1 = np.array(matrix1[:]).reshape(size, size, size)
-    matrix2 = np.array(matrix2[:]).reshape(size, size, size)
-    """
+
+    result_matrix = np.frombuffer(result, dtype=np.int32).reshape(size, size, size)
+    
     print(matrix1)
     print("--------------------------")
     print(matrix2)
     print("--------------------------")
     print(result_matrix)
-    """
 
-if __name__ == "__main__":
-    main()
